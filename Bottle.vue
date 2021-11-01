87625a6d3244230ca3ec84c5b26718cb24e2a376
Joaquim Perez <jivkim@gmail.com>
2021-11-01 13:59:12 +0100

    use hammerjs to add or remove wine on panup pandown

--- Bottle.vue
+++ Bottle.vue
@@ -1,5 +1,9 @@
 <template>
-  <svg style="width:240px;height:240px;paint-order:stroke" :viewBox="`0 0 ${size} ${size}`">
+  <svg
+    style="width:240px;height:240px;paint-order:stroke"
+    :viewBox="`0 0 ${size} ${size}`"
+    ref="bottleSvg"
+  >
     <mask id="bottlemask">
       <rect fill="black" :width="size" :height="size" />
       <path fill="white" :d="path" />
@@ -10,6 +14,8 @@
 </template>
 
 <script>
+import Hammer from 'hammerjs'
+
 export default {
   data: () => ({
     path: 'M10,22A1,1 0 0,1 9,21V11C9,9 10,7.25 11,7V2.5A0.5,0.5 0 0,1 11.5,2H12.5A0.5,0.5 0 0,1 13,2.5V7C14,7.25 15,9 15,11V21A1,1 0 0,1 14,22H10Z',
@@ -20,6 +26,18 @@ export default {
   computed: {
     y() {
       return this.size * this.ratio
+    }
+  },
+
+  mounted () {
+    let mc = Hammer(this.$el)
+    mc.get('pan').set({ direction: Hammer.DIRECTION_VERTICAL })
+    mc.on('panup pandown', e => this.add(e.velocityY))
+  },
+
+  methods: {
+    add (velocity) {
+      this.ratio = this.ratio - velocity/80
     }
   }
 }
